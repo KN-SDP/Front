70962a61e271dce2f426ab0131a1dbdb52991f58
chunsh02 <trugen7932@naver.com>
2025-10-02 19:51:20 +0900

    Refactor: App.js에서 createStackNavigator 삭제 후 native-stack으로 통일 작업

--- App.js
+++ App.js
@@ -1,32 +1,22 @@
-// App.js
 import React from 'react';
-import { Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createStackNavigator } from '@react-navigation/stack';
-
-// 선택: 테마/Provider 생략하고 최소 구성으로 안정화부터
 import Login from './Login';
 import SignUp from './SignUp';
-import FindId from './FindId';
+import FindId from './findId';
 
-// 웹은 기본 Stack, 네이티브는 Native Stack
-const Stack = Platform.OS === 'web'
-  ? createStackNavigator()
-  : createNativeStackNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
         initialRouteName="Login"
-        screenOptions={{
-          headerShown: Platform.OS === 'web', // 필요시 헤더 on/off
-        }}
+        screenOptions={{ headerShown: false }}
       >
-        <Stack.Screen name="Login" component={Login} options={{ title: '' }} />
-        <Stack.Screen name="SignUp" component={SignUp} options={{ title: '' }} />
-        <Stack.Screen name="FindId" component={FindId} options={{ title: '' }} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="findId" component={FindId} />
       </Stack.Navigator>
     </NavigationContainer>
   );
