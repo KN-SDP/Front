f8c4f20d7dde3dcecbda60749e8d98ad7e6b72fe
chunsh02 <trugen7932@naver.com>
2025-11-12 17:55:49 +0900

    Fix: 클라우드 타입 배포 코드 추가

--- index.js
+++ index.js
@@ -2,15 +2,21 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { registerRootComponent } from 'expo';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Platform } from 'react-native';
 import App from './App';
 
+// ✅ 웹이 아닐 때만 GestureHandlerRootView 적용
 function Root() {
-  return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <App />
-    </GestureHandlerRootView>
-  );
+  if (Platform.OS === 'web') {
+    return <App />;
+  } else {
+    const { GestureHandlerRootView } = require('react-native-gesture-handler');
+    return (
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <App />
+      </GestureHandlerRootView>
+    );
+  }
 }
 
 registerRootComponent(Root);
